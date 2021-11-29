bd88425c57c7bd08a3fc504568047570c5477f0f
Mason <bigmasonwang@gmail.com>
2021-11-29 16:44:09 +1030

    static page

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,10 +4,18 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title", "Home | RoR Simple App"
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title", "Help | RoR Simple App"
+  end
+
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+    assert_select "title", "About | RoR Simple App"
   end
 end
