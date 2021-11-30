1bf023de2853fb260ae772c9779c7c00adcbeb8f
Mason <bigmasonwang@gmail.com>
2021-11-30 15:41:28 +1030

    rails router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help' 
+  get '/about', to: 'static_pages#about' 
+  get '/contact', to: 'static_pages#contact'
 end
