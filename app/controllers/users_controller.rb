c4608db10020e06b587be38ba4bc8b653e3afaf0
Mason <bigmasonwang@gmail.com>
2021-12-01 15:32:27 +1030

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_prams)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_prams
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
